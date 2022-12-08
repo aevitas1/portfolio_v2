84f5a3b3a33f549ea9458226bd45ce2c3a5f8158
Stephan <obvilion@gmail.com>
2022-12-08 22:33:34 +0100

    Cleaned up some files

--- src/components/ContentWrapper/Content/Pages/Projects.jsx
+++ src/components/ContentWrapper/Content/Pages/Projects.jsx
@@ -1,96 +1,26 @@
 import Card from "../Card";
-import foodfreedom from '../../../../assets/projects/Screenshot 2022-11-29 at 00.52.27.png';
+import foodfreedom from "../../../../assets/projects/Screenshot 2022-11-29 at 00.52.27.png";
 
 const Projects = () => {
-    return (
-        <>
-            <h1>Projects</h1>
-            <div className="project_wrapper">
-                <Card image={foodfreedom}
-                      projectName="Foodfreedom"
-                      description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      bootstrap={true}
-                      html={true}
-                      className='first'
-                      id={1}
-                />
-
-                <Card image={foodfreedom}
-                      projectName="Memory game"
-                      description="The final project I made for my study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      html={true}
-                      css={true}
-                      className='first'
-                />
-
-                <Card image={foodfreedom}
-                      projectName="Portfolio"
-                      description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      html={true}
-                      react={true}
-                      sass={true}
-                      className='first'
-                />
-
-                <Card image={foodfreedom}
-                      projectName="Portfolio"
-                      description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      html={true}
-                      react={true}
-                      sass={true}
-                      className='first'
-                />
-
-                <Card image={foodfreedom}
-                      projectName="Portfolio"
-                      description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      html={true}
-                      react={true}
-                      sass={true}
-                      className='first'
-                />
-
-                <Card image={foodfreedom}
-                      projectName="Portfolio"
-                      description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      html={true}
-                      react={true}
-                      sass={true}
-                      className='first'
-                />
-
-                <Card image={foodfreedom}
-                      projectName="Portfolio"
-                      description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
-                      projectLink="https://aevitas1.github.io/"
-                      codeLink="https://aevitas1.github.io/"
-                      javascript={true}
-                      html={true}
-                      react={true}
-                      sass={true}
-                      className='first'
-                />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Projects</h1>
+      <div className="project_wrapper">
+        <Card
+          image={foodfreedom}
+          projectName="Foodfreedom"
+          description="The final project I made for my HTML/CSS study. The final project I made for my HTML/CSS study."
+          projectLink="https://aevitas1.github.io/"
+          codeLink="https://aevitas1.github.io/"
+          javascript={true}
+          bootstrap={true}
+          html={true}
+          className="first"
+          id={1}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Projects;
