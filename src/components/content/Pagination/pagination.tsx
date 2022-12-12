051ad34c9f2b9c2039402e0484c5d8f3f9d6141a
Stephan <obvilion@gmail.com>
2022-12-12 23:14:11 +0100

    Added href attribute to pagelinks

--- src/components/content/Pagination/pagination.tsx
+++ src/components/content/Pagination/pagination.tsx
@@ -14,34 +14,73 @@ export default function Pagination({
   currentPage,
   lastPage,
   maxLength,
-  setCurrentPage,
 }: Props) {
   const pageNums = getPaginationItems(currentPage, lastPage, maxLength);
+  const baseURL = "/projects";
 
   return (
     <nav className={styles.pagination} aria-label="Pagination">
       <PageLink
         disabled={currentPage === 1}
-        onClick={() => setCurrentPage(currentPage - 1)}
+        href={`${baseURL}/${currentPage - 1}`}
+        onClick={() => currentPage - 1}
       >
         Previous
       </PageLink>
       {pageNums.map((pageNum, idx) => (
         <PageLink
           key={idx}
+          href={`${baseURL}/${pageNum}`}
           active={currentPage === pageNum}
           disabled={isNaN(pageNum)}
-          onClick={() => setCurrentPage(pageNum)}
+          onClick={() => pageNum}
         >
           {!isNaN(pageNum) ? pageNum : "..."}
         </PageLink>
       ))}
       <PageLink
+        href={`${baseURL}/${currentPage + 1}`}
         disabled={currentPage === lastPage}
-        onClick={() => setCurrentPage(currentPage + 1)}
+        onClick={() => currentPage + 1}
       >
         Next
       </PageLink>
     </nav>
   );
 }
+
+// export default function Pagination({
+//   currentPage,
+//   lastPage,
+//   maxLength,
+//   setCurrentPage,
+// }: Props) {
+//   const pageNums = getPaginationItems(currentPage, lastPage, maxLength);
+
+//   return (
+//     <nav className={styles.pagination} aria-label="Pagination">
+//       <PageLink
+//         disabled={currentPage === 1}
+//         onClick={() => setCurrentPage(currentPage - 1)}
+//       >
+//         Previous
+//       </PageLink>
+//       {pageNums.map((pageNum, idx) => (
+//         <PageLink
+//           key={idx}
+//           active={currentPage === pageNum}
+//           disabled={isNaN(pageNum)}
+//           onClick={() => setCurrentPage(pageNum)}
+//         >
+//           {!isNaN(pageNum) ? pageNum : "..."}
+//         </PageLink>
+//       ))}
+//       <PageLink
+//         disabled={currentPage === lastPage}
+//         onClick={() => setCurrentPage(currentPage + 1)}
+//       >
+//         Next
+//       </PageLink>
+//     </nav>
+//   );
+// }
