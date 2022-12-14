2c2d291cf2be66e6eb10b052044acbe2290b050c
Stephan <obvilion@gmail.com>
2022-12-14 11:46:36 +0100

    Fixed VH issue, added host address to debug on phone

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import Pagewrapper from "./components/wrapper/PageWrapper";
 import { AppProvider } from "./context/AppContext";
 
 function App() {
+  // Mobile 100vh fix to avoid vertical scrollbar
+  const calculateVh = () => {
+    let vh = window.innerHeight;
+    // Set this as a variable
+    document.documentElement.style.setProperty("--vh", vh + "px");
+  };
+
+  // Initial calc
+  calculateVh();
+
+  // Recalculate on resize
+  window.addEventListener("resize", calculateVh);
+
+  // Recalculate on device orientation change
+  window.addEventListener("orientationchange", calculateVh);
+
   return (
     <>
       <AppProvider>
